6f36ee6fa9c11dfd623b0645375fc8a90a940efd
m_adriani <m.adriani@jdk.it>
2024-07-05 10:18:44 +0200

    nuovi test crud

--- src/main/java/com/example/mockitoMojito/student/StudenteService.java
+++ src/main/java/com/example/mockitoMojito/student/StudenteService.java
@@ -5,6 +5,8 @@ import com.example.mockitoMojito.dto.StudenteDTOResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class StudenteService {
 
@@ -13,10 +15,25 @@ public class StudenteService {
     @Autowired
     private StudenteMapper studenteMapper;
 
+    public StudenteDTOResponse getStudenteById(Long id) {
+        return studenteMapper.dtoFromStudente(studenteRepository.findById(id).orElse(null));
+    }
+
+    public List<StudenteDTOResponse> getAll() {
+        return studenteRepository.findAll()
+                .stream()
+                .map(studenteMapper::dtoFromStudente)
+                .toList();
+    }
+
     public StudenteDTOResponse saveStudente(StudenteDTO studenteDTO) {
         Studente studente = studenteMapper.studenteFromDTO(studenteDTO);
         Studente savedStudente = studenteRepository.save(studente);
         return studenteMapper.dtoFromStudente(savedStudente);
+    }
+
+    public void deleteById(Long id) {
+        studenteRepository.deleteById(id);
     }
 
 
--- src/test/java/com/example/mockitoMojito/student/StudenteServiceTest.java
+++ src/test/java/com/example/mockitoMojito/student/StudenteServiceTest.java
@@ -11,7 +11,9 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 class StudenteServiceTest {
 
@@ -28,7 +30,7 @@ class StudenteServiceTest {
     }
 
     @Test
-    public void testDaveStudente() {
+    public void testSaveStudente() {
         // given
         StudenteDTO studenteDTO = StudenteDTO.builder()
                 .nome("Homer")
@@ -80,4 +82,114 @@ class StudenteServiceTest {
         Mockito.verify(studenteRepository, Mockito.times(1)).save(studente);
         Mockito.verify(studenteMapper, Mockito.times(1)).dtoFromStudente(savedStudente);
     }
+
+    @Test
+    public void testGetById() {
+        // given
+        Long id = 1L;
+        Studente studente = Studente.builder()
+                .nome("Homer")
+                .cognome("Simpson")
+                .eta(40)
+                .email("flanders@mail.com")
+                .university(new University())
+                .build();
+
+        StudenteDTOResponse studenteDTOResponse = StudenteDTOResponse.builder()
+                .id(id)
+                .nome("Homer")
+                .cognome("Simpson")
+                .eta(40)
+                .email("flanders@mail.com")
+                .build();
+
+        // mock delle chiamate
+        Mockito.when(studenteRepository.findById(id)).thenReturn(Optional.of(studente));
+        Mockito.when(studenteMapper.dtoFromStudente(studente)).thenReturn(studenteDTOResponse);
+
+        // when
+        StudenteDTOResponse result = studenteService.getStudenteById(id);
+
+        // then
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(result.getNome(), studenteDTOResponse.getNome());
+        Assertions.assertEquals(result.getCognome(), studenteDTOResponse.getCognome());
+        Assertions.assertEquals(result.getEmail(), studenteDTOResponse.getEmail());
+        Assertions.assertEquals(result.getEta(), studenteDTOResponse.getEta());
+
+        Mockito.verify(studenteMapper, Mockito.times(1)).dtoFromStudente(studente);
+        Mockito.verify(studenteRepository, Mockito.times(1)).findById(id);
+
+    }
+
+    @Test
+    public void testGetAll() {
+        // given
+        List<Studente> students = new ArrayList<>();
+        Studente studente = Studente.builder()
+                .nome("Homer")
+                .cognome("Simpson")
+                .eta(40)
+                .email("flanders@mail.com")
+                .university(new University())
+                .build();
+        Studente anotherStudente = Studente.builder()
+                .nome("Peter")
+                .cognome("Griffin")
+                .eta(44)
+                .email("peter@gmail.com")
+                .university(new University())
+                .build();
+
+        students.add(studente);
+        students.add(anotherStudente);
+
+        // mock delle chiamate
+        Mockito.when(studenteRepository.findAll()).thenReturn(students);
+        Mockito.when(studenteMapper.dtoFromStudente(Mockito.any(Studente.class)))
+                .thenAnswer(invocationOnMock -> {
+                    Studente result = invocationOnMock.getArgument(0);
+                    return StudenteDTOResponse.builder()
+                            .id(result.getId())
+                            .nome(result.getNome())
+                            .cognome(result.getCognome())
+                            .email(result.getEmail())
+                            .eta(result.getEta())
+                            .build();
+                });
+
+        // when
+        List<StudenteDTOResponse> result = studenteService.getAll();
+
+        // then
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(result.size(), 2);
+        Assertions.assertEquals(result.getFirst().getNome(), "Homer");
+        Assertions.assertEquals(result.getFirst().getCognome(), "Simpson");
+        Assertions.assertEquals(result.get(0).getEta(), 40);
+        Assertions.assertEquals(result.get(0).getEmail(), "flanders@mail.com");
+
+        Assertions.assertEquals(result.get(1).getNome(), "Peter");
+        Assertions.assertEquals(result.get(1).getCognome(), "Griffin");
+        Assertions.assertEquals(result.get(1).getEta(), 44);
+        Assertions.assertEquals(result.get(1).getEmail(), "peter@gmail.com");
+
+        Mockito.verify(studenteRepository, Mockito.times(1)).findAll();
+        Mockito.verify(studenteMapper, Mockito.times(2)).dtoFromStudente(Mockito.any(Studente.class));
+
+    }
+
+    @Test
+    public void testDeleteById() {
+        // given
+        Long idStudente = 1L;
+
+        // when
+        studenteService.deleteById(idStudente);
+
+        // then
+        Assertions.assertNull(studenteService.getStudenteById(idStudente));
+        Mockito.verify(studenteRepository, Mockito.times(1)).deleteById(idStudente);
+    }
+
 }
